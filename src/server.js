d89e51abec9a62640648c113a8d6ff28dca1b645
JanEveraertEHB <72449780+JanEveraertEHB@users.noreply.github.com>
2020-10-20 13:59:00 +0200

    commit main

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-console.log("starting up")
+console.log("starting up - main")
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
