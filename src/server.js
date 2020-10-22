0358ebdaa9f110b4b345f3a9c30fecdacb13c58e
JanEveraertEHB <72449780+JanEveraertEHB@users.noreply.github.com>
2020-10-22 09:23:49 +0200

    fix(server): console log removal

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-console.log("starting up - main")
+//console.log("starting up - main")
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+// adding pong
 app.get('/ping', (req, res) => {
   res.send('Hello pong!')
 })
